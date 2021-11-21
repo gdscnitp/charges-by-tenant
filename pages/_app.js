c701be6026e7d5b0e255828387844ed2f6338a39
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2021-11-21 18:28:49 +0530

    Added custom logger

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import logger from '../helpers/logger';
 
 function MyApp({ Component, pageProps }) {
+  
   return <Component {...pageProps} />
+}
+
+MyApp.getInitialProps = async (ctx) => {
+ 
+  const areLogsEnabled = ctx?.router?.query?.debug || '';
+	    global.areLogsEnabled = areLogsEnabled === 'true';
+  return {};
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 
 export default function Home() {
+  
   return (
     <div className={styles.container}>
       <Head>
