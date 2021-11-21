a1fb3bf832bea7733ddc9e591283c4ff9401a35c
rvr007-glitch <79240272+rvr007-glitch@users.noreply.github.com>
2021-11-21 17:04:18 +0530

    Database connection through mongoose

--- db/db.js
+++ db/db.js
@@ -1,39 +1,4 @@
-import { MongoClient } from 'mongodb'
 
-let uri = process.env.MONGODB_URI
-let dbName = process.env.MONGODB_DB
-
-let cachedClient = null
-let cachedDb = null
-
-if (!uri) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
-}
-
-if (!dbName) {
-  throw new Error(
-    'Please define the MONGODB_DB environment variable inside .env.local'
-  )
-}
-
-export async function connectToDatabase() {
-  if (cachedClient && cachedDb) {
-    return { client: cachedClient, db: cachedDb }
-  }
-
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-
-  }
-  )
-  console.log("connected")
-  const db = await client.db(dbName)
-
-  cachedClient = client
-  cachedDb = db
-
-  return { client, db }
-}
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("db connected")).catch((err) => { console.log(err) });
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { connectToDatabase } from "../../db/db"
-export default async function handler(req, res) {
- 
-  const { db } = await connectToDatabase();
-  console.log(db);
-  const data = await db.collection("startup_log").find({}).limit(20).toArray();
-  res.status(200).json(data)
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
 }
