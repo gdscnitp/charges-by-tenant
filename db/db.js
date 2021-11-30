ac3b9a4badf704a0437c67c1de9374345abd38e6
rvr007-glitch <79240272+rvr007-glitch@users.noreply.github.com>
2021-11-30 14:08:52 +0530

    error handling

--- db/db.js
+++ db/db.js
@@ -1,4 +1,21 @@
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("db connected")).catch((err) => { console.log(err) });
+mongoose.set("useCreateIndex",true);
+connectMongodb();
+
+
+mongoose.connection.on("error",function(err){
+  console.trace("Mongodb connection Failed",err);
+  
+});
+mongoose.connection.on("connected",function(success){
+  console.log("successsfully opened mongodb connection");
+})
+function connectMongodb(){
+  mongoose.connect(process.env.MONGODB_URI,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify :false,
+  })
+}
