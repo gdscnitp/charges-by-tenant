91b6ed9134050e3bf93a885bd6da339f8b1a1065
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2021-11-30 17:52:34 +0530

    Added helpers for response

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
 }
 
 MyApp.getInitialProps = async (ctx) => {
- 
+  
   const areLogsEnabled = ctx?.router?.query?.debug || '';
 	    global.areLogsEnabled = areLogsEnabled === 'true';
   return {};
