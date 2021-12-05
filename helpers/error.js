0a7cfa96f07b040c28fc80d78229f0d9b4adbde2
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2021-12-05 12:30:04 +0530

    added error and constants file

---
+++ helpers/error.js
@@ -0,0 +1,13 @@
+var errors = {
+
+//tenants error
+tenant_name_taken: [400,"Tenant name already taken"],
+
+//landlord error
+landlord_name_taken: [400,"Landlord name already taken"]
+
+
+
+}
+
+module.exports = errors;
