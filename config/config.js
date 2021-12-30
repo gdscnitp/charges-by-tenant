07cc5aab84437b5feab7d154897eb572f3905e30
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-12-30 12:40:08 +0530

    Delete config.js

--- config/config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-    MONGO_URL: `mongodb+srv://admin:jkdbsfIDHSO@cluster0.heazp.mongodb.net/charges-by-tenants?retryWrites=true&w=majority`,
-    DB_CONNECTION_RETTEMPT_LIMIT_NODE: 5,
-    DB_USERNAME: "admin",
-    DB_PASSWORD: "jkdbsfIDHSO"
-}
-
-module.exports = config;
