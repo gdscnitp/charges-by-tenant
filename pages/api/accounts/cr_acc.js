cf915db5202d7e9a553e50cd16a9b961bcb3c6b8
Rishabh Mishra <rishabhborn2win@gmail.com>
2021-12-30 12:03:35 +0530

    routing check

--- pages/api/accounts/cr_acc.js
+++ pages/api/accounts/cr_acc.js
@@ -1,7 +1,15 @@
-import { sendError } from "next/dist/server/api-utils";
+import { sendError } from "../../../helpers/help";
 import { sendSuccess } from "../../../helpers/help";
 
 export default function handler(req, res) {
 
+    if(req.method==="POST")
+    {
+      var {name, email} = req.body;
+      var data = {}
+      name? data.name = name : "";
+      email? data.email = email : "";
+      return sendSuccess(res,data)
+    }
     return sendError(res,"checking error", 500)
   }
