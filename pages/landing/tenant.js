1c425ed5566ae4a485184c8974970be662b1e4b9
SHIVAM3223 <shivambhushan653@gmail.com>
2022-01-01 15:39:47 +0530

    completed landing page

--- pages/landing/tenant.js
+++ pages/landing/tenant.js
@@ -9,7 +9,11 @@ const Tenant = () => {
             <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
             <Identity />
             <div className="S_right">
+                <div className="S_head">Your sites</div>
                 <HorizontalLine />
+                <LandingPageCard />
+                <LandingPageCard />
+                <LandingPageCard />
                 <LandingPageCard />
             </div>
         </div>
