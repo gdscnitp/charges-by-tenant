514fe64fdec7b399ae75c760509cc2d00e324c4b
SHIVAM3223 <shivambhushan653@gmail.com>
2022-01-01 15:23:45 +0530

    corrected class to className

--- pages/homepage/components/FeatureCard.js
+++ pages/homepage/components/FeatureCard.js
@@ -1,6 +1,6 @@
 function FeatureCard() {
   return (
-    <div class="col-sm-3 px-2 py-2 a-feature-card">
+    <div className="col-sm-3 px-2 py-2 a-feature-card">
       <div className="card bg-light mb-3 shadow">
         <h5 className="card-title m-2">Get Notified</h5>
         <div className="card-body">
