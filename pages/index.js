bbecf2598e4613acc37665e7fdd5f0defa9c70bc
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2022-01-02 14:05:48 +0530

    Removed some errors of class instead of className

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import HomePage from "./HomePage";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    // <div className={styles.container}>
-    //   <Head>
-    //     <title>Page Title</title>
-    //   </Head>
-    //   <div className={styles.heading}>Charges By Tenants Home Page!!</div>
-    // </div>
-    <HomePage />
+    <div>
+      <HomePage />
+    </div>
   );
 }
