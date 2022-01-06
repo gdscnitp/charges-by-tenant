a5e809f4e07a5ece4b6734b926291f1565ead5d6
SHIVAM3223 <shivambhushan653@gmail.com>
2022-01-06 16:03:35 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 let config = {
-    MONGO_URL: `mongodb+srv://admin:jkdbsfIDHSO@cluster0.heazp.mongodb.net/charges-by-tenants?retryWrites=true&w=majority`,
+    MONGO_URL: `mongodb+srv://admin:admin@cluster0.evrtj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     DB_CONNECTION_RETTEMPT_LIMIT_NODE: 5,
     SECRET_KEY: "jksdo"
 }
