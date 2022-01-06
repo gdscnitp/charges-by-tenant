d04698f35c6ef9dc1e5eae5aee4a312f3a634bdd
SHIVAM3223 <shivambhushan653@gmail.com>
2022-01-06 14:03:25 +0530

    passed class props for Heading

--- pages/landing/components/Heading.js
+++ pages/landing/components/Heading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = (props) => {
     return (
         <div>
-            <div className="S_head">{props.head}</div>
+            <div className={`S_head ${props.headClass}`}>{props.head}</div>
         </div>
     )
 }
