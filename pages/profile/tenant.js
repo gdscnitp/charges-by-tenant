3df605a2ee3b9e728e5b34d429e8d73f264936d1
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2022-01-14 01:10:55 +0530

    added map functionality

--- pages/profile/tenant.js
+++ pages/profile/tenant.js
@@ -3,6 +3,64 @@ import TableList from "./components/TableList";
 import Taskbar from "./components/Taskbar";
 import Header from "./components/Header";
 
+const tableData = [
+  // Comment delete mat karna apshabd
+  // {
+  //   FlatNo:
+  //   Addres:
+  //   AliasName:
+  //   PataNahi:
+  // },
+  {
+    col1: "Flat-402",
+    col2: "Near Road, XYZ Town, ABC",
+    col3: "Dummy",
+    col4: "Residential",
+  },
+  {
+    col1: "Flat-402",
+    col2: "Near Road, XYZ Town, ABC",
+    col3: "Dummy",
+    col4: "Residential",
+  },
+  {
+    col1: "Flat-402",
+    col2: "Near Road, XYZ Town, ABC",
+    col3: "Dummy",
+    col4: "Residential",
+  },
+  {
+    col1: "Flat-402",
+    col2: "Near Road, XYZ Town, ABC",
+    col3: "Dummy",
+    col4: "Residential",
+  },
+  {
+    col1: "Flat-402",
+    col2: "Near Road, XYZ Town, ABC",
+    col3: "Dummy",
+    col4: "Residential",
+  },
+  {
+    col1: "Flat-402",
+    col2: "Near Road, XYZ Town, ABC",
+    col3: "Dummy",
+    col4: "Residential",
+  },
+  {
+    col1: "Flat-402",
+    col2: "Near Road, XYZ Town, ABC",
+    col3: "Dummy",
+    col4: "Residential",
+  },
+  {
+    col1: "Flat-402",
+    col2: "Near Road, XYZ Town, ABC",
+    col3: "Dummy",
+    col4: "Residential",
+  },
+];
+
 export default function Home() {
   return (
     <div className="Parent">
@@ -18,14 +76,26 @@ export default function Home() {
         <hr />
         <div className="S_rightBottom">
           <Header head="Available Sites" />
+
           <TableList
+            tableclass="table-striped Stable"
+            flat="Flat No."
+            loc="Address"
+            siteName="Site Name"
+            available="Type"
+            view="View"
+            tableData={tableData}
+          />
+
+          {/* <TableList
             tableclass="table-striped Stable"
             flat="Flat-402"
             loc="Near Road, XYZ Town, ABC"
             siteName="From: 04 Dec 2021"
             available="Residential"
             view="View"
-          />
+            tableData={tableData}
+          /> */}
         </div>
       </div>
     </div>
