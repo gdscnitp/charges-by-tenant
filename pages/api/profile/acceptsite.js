e9266fdd3b5df0120e18be620660bda20ada1276
Shruti12130 <shrutijai01@gmail.com>
2022-01-24 20:42:33 +0530

    Accept the Request for the site by tenant Modification

--- pages/api/profile/acceptsite.js
+++ pages/api/profile/acceptsite.js
@@ -6,24 +6,57 @@ var History = require("../../../models/history")
 
 export default async function handler(req,res){
 
-if(req.method === "PUT"){
-var histId = '61e53b40259ba909fbe0bb14'  //history id
+    if(req.method === "PUT"){
+        var histId = req.body.histId  //history id
+        //var siteId=req.body.siteId;
 
-var histData = await History.findByIdAndUpdate(histId,{$set:{status:"1",joined_at:Date.now()}})
-if(histData){
-var siteData = await Sites.findByIdAndUpdate(histData.site_id,{$set:{status:"2",tenant:histData.tenant_id}}) 
-if(siteData){
-    return sendSuccess(res,{})
-}
-else{
-    return sendError(res,"no such site",500)
-}
-}
-else{
-    return sendError(res,"no such history",500)
-}
+        History.findById(histId, function(err, histData){
+            if(err) return sendError(res, err.message, 500)
+            else{
+                const bearerHeader = req.headers['authorization'];
+                if(typeof bearerHeader !== 'undefined') {
+                const bearer = bearerHeader.split(' ');
+                const bearerToken = bearer[1];
+                req.token = bearerToken;
+                
+                jwt.verify(req.token, config.SECRET_KEY, (err, authData) => {
+                    if(err)
+                    return sendError(res,err,constants.JWT_VERIFY)
+                    else{
+                        if(histData.tenant_id == tenant._id){
+                            if(histData.status==="0"){
+                                var histData = await History.findByIdAndUpdate(histId,{$set:{status:"1",joined_at:Date.now()}})
+                                if(histData){
+                                    var siteData = await Sites.findByIdAndUpdate(histData.site_id,{$set:{status:"2",tenant:histData.tenant_id}}) 
+                                    if(siteData){
+                                        return sendSuccess(res,{})
+                                    }else{
+                                        return sendError(res,"No such site",500)
+                                    }
+                                }else{
+                                    return sendError(res,"No such history",500)
+                                }
+                               
+                            }else{
+                                return sendError(res,err.message,500)
+                            }
+                        }
+                        else{
+                            return sendError(res, "Unauthorized Access", 500)
+                        }
+                    }
+                })
+                
+                } else {
+                // Forbidden
+                return sendError(res,"Token not provided", constants.NULL_TOKEN)
+                }
 
-//return sendError(res,"finding error",constants.SERVER_ERROR)
-}
-
+            
+            }
+        })
+    } 
+    else{
+        return sendError(res, err.message, 500)
+    }
 }
