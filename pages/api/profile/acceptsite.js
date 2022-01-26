dc7eb6b08d107074445ab6759404d15688142f74
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2022-01-26 22:17:34 +0530

    changed api/profile code
    Changed /history to /History in  import of var History

--- pages/api/profile/acceptsite.js
+++ pages/api/profile/acceptsite.js
@@ -1,29 +1,29 @@
 import { sendSuccess, sendError } from "../../../helpers/help";
-var constants = require("../../../helpers/constants")
+var constants = require("../../../helpers/constants");
 import connectMongoDb from "../../../db/connect";
-var Sites = require("../../../models/Sites")
-var History = require("../../../models/History")
+var Sites = require("../../../models/Sites");
+var History = require("../../../models/History");
 
-export default async function handler(req,res){
+export default async function handler(req, res) {
+  if (req.method === "PUT") {
+    var histId = "61e53b40259ba909fbe0bb14"; //history id
 
-if(req.method === "PUT"){
-var histId = '61e53b40259ba909fbe0bb14'  //history id
+    var histData = await History.findByIdAndUpdate(histId, {
+      $set: { status: "1", joined_at: Date.now() },
+    });
+    if (histData) {
+      var siteData = await Sites.findByIdAndUpdate(histData.site_id, {
+        $set: { status: "2", tenant: histData.tenant_id },
+      });
+      if (siteData) {
+        return sendSuccess(res, {});
+      } else {
+        return sendError(res, "no such site", 500);
+      }
+    } else {
+      return sendError(res, "no such history", 500);
+    }
 
-var histData = await History.findByIdAndUpdate(histId,{$set:{status:"1",joined_at:Date.now()}})
-if(histData){
-var siteData = await Sites.findByIdAndUpdate(histData.site_id,{$set:{status:"2",tenant:histData.tenant_id}}) 
-if(siteData){
-    return sendSuccess(res,{})
-}
-else{
-    return sendError(res,"no such site",500)
-}
-}
-else{
-    return sendError(res,"no such history",500)
-}
-
-//return sendError(res,"finding error",constants.SERVER_ERROR)
-}
-
+    //return sendError(res,"finding error",constants.SERVER_ERROR)
+  }
 }
--- pages/api/profile/viewsite.js
+++ pages/api/profile/viewsite.js
@@ -1,46 +1,45 @@
 import { sendSuccess, sendError } from "../../../helpers/help";
-var constants = require("../../../helpers/constants")
+var constants = require("../../../helpers/constants");
 import connectMongoDb from "../../../db/connect";
-var History = require("../../../models/History")
-const {isEmail, isDate} = require("validator");
-const jwt = require("jsonwebtoken")
+var History = require("../../../models/history");
+const { isEmail, isDate } = require("validator");
+const jwt = require("jsonwebtoken");
 var nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
-const config = require("../../../config/config")
+const config = require("../../../config/config");
 
 //const {isEmail} = require("validator");
 //var nodemailer = reuire("nodemailer");
-export default async function handler(req,res){
-    if(req.method === "GET"){
-        const bearerHeader = req.headers['authorization'];
-        // Check if bearer is undefined
-        if(typeof bearerHeader !== 'undefined') {
-          // Split at the space
-          const bearer = bearerHeader.split(' ');
-          // Get token from array
-          const bearerToken = bearer[1];
-          // Set the token
-          req.token = bearerToken;
-          // Next middleware
-          jwt.verify(req.token, config.SECRET_KEY, (err,authData) => {
-            
-            if(err)return sendError(res,err,constants.JWT_VERIFY)
-            else{
-                History.find({tenant_id:authData.id}).populate('site_id').exec(function(err,data){
-                  if(err) return sendError(res,err.message,constants.HISTORY_ERROR_I)
-                  else return sendSuccess(res,{data})
-              })
-            }
-        })
-          
-        } else {
-          // Forbidden
-          //currently not possible because we are loged in and token is available to us
-          return sendError(res,"token not availanle",constants.NULL_TOKEN)
-        } 
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const bearerHeader = req.headers["authorization"];
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== "undefined") {
+      // Split at the space
+      const bearer = bearerHeader.split(" ");
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      jwt.verify(req.token, config.SECRET_KEY, (err, authData) => {
+        if (err) return sendError(res, err, constants.JWT_VERIFY);
+        else {
+          History.find({ tenant_id: authData.id })
+            .populate("site_id")
+            .exec(function (err, data) {
+              if (err)
+                return sendError(res, err.message, constants.HISTORY_ERROR_I);
+              else return sendSuccess(res, { data });
+            });
+        }
+      });
+    } else {
+      // Forbidden
+      //currently not possible because we are loged in and token is available to us
+      return sendError(res, "token not availanle", constants.NULL_TOKEN);
     }
-
-    else{
-        return sendError(res,"Use get method",constants.USE_GET)
-    }
+  } else {
+    return sendError(res, "Use get method", constants.USE_GET);
+  }
 }
