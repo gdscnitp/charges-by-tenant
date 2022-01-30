eb9f043e9b8bc02a0d3c704cc55b97704f49e08f
Rishabh Mishra <rishabhborn2win@gmail.com>
2022-01-30 19:27:47 +0530

    Leave site route added

--- pages/api/profile/leavesite.js
+++ pages/api/profile/leavesite.js
@@ -1,36 +1,68 @@
-// import { sendSuccess, sendError } from "../../../helpers/help";
-// var constants = require("../../../helpers/constants")
-// import connectMongoDb from "../../../db/connect";
-// //import sites from "../../../models/sites";
-// //import { ApiError } from "next/dist/server/api-utils";
-// var Tenant = require("../../../models/Tenant")
-// var Site = require("../../../models/Site")
-// var History = require("../../../models/History")
-// const {isEmail, isDate} = require("validator");
-// const jwt = require("jsonwebtoken")
-// var nodemailer = require("nodemailer");
-// const bcrypt = require("bcrypt");
-// const config = require("../../../config/config")
+import { sendSuccess, sendError } from "../../../helpers/help";
+var constants = require("../../../helpers/constants");
+import connectMongoDb from "../../../db/connect";
+var config = require("../../../config/config")
+import {auth} from "../../../utility/auth"
+var Site = require("../../../models/Site");
+const {isEmail, isMongoId} = require("validator")
+var History = require("../../../models/history");
+var Sites  = require("../../../models/Site")
 
-// export default async function handler(req,res){
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    var histId = req.body.histId; //history id
 
-// if(req.method === "PUT"){
-// var histId = '61e53b40259ba909fbe0bb14'  //history id   
+    if(!histId){
+        return sendError(res,"histId not provided")
+    }
 
-// var histData = await History.findByIdAndUpdate(histId,{$set:{status:"2",left_at:Date.now()}})
-// if(histData){
-// var siteData = await Site.findByIdAndUpdate(histData.site_id,{$set:{status:"0"}}) 
-// if(siteData){
-//     return sendSuccess(res,{})
-// }
-// else{
-//     return sendError(res,"no such site",500)
-// }
-// }
-// else{
-//     return sendError(res,"no such history",500)
-// }
+    if(!isMongoId(histId)){
+        return sendError(res,"invalid histId",500)
+    }
 
-// }
 
-// }
+      //Leave the site means change the status of the history and status of the site
+      History.findById(histId, function(err,histData){
+        console.log(histData)
+        if(err) return sendError(res,err.message,500)
+        else if(histData === null)return sendError(res,"the history you are looking for dosen't exist",500)
+        else{
+            var tenantId;
+            auth(req,res,(err,Data) => {
+                if(err) return sendError(res,err.message,500)
+                tenantId = Data.id
+            })
+
+            if(tenantId == histData.tenant_id){
+                if(histData.status === "1"){
+                    History.findByIdAndUpdate(histId, {$set: {left_at: Date.now(), status: "2"}}, function(err, histNewData){
+                        if(err) return sendError(res, err.message, 500)
+                        else{
+                            Sites.findByIdAndUpdate(histData.site_id, {$set: {status:"0"}}, function(err, d){
+                                if(err) return sendError(res, err.message, 500)
+                                else{
+                                    History.findById(histId, function(err, data ){
+                                        if(err) return sendError(res, err.message, 500)
+                                        else{
+                                            return sendSuccess(res, data)
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }
+                else{
+                    return sendError(res,"Request For Site Site is not accepetd or you already left",500)
+                }
+            }
+            else{
+                return sendError(res,"Unauthorized",500)
+            }
+        }
+    })
+  }
+  else{
+      return sendError(res, "Please select POST method", 500)
+  }
+}
