37593d84364fdb6fcec2b85de3d0b8173667a01a
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2022-03-29 01:06:23 +0530

    bell icon

--- pages/landing/components/Identity.js
+++ pages/landing/components/Identity.js
@@ -36,12 +36,6 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const Identity = (props) => {
-  const [req, setReq] = useState(0);
-  useEffect(() => {
-    setReq(props.getCount());
-  }, []);
-  // }, [props.getCount()]);
-
   return (
     <div className="S_landing">
       <div className="S_leftbar">
@@ -63,11 +57,11 @@ const Identity = (props) => {
           <div className="S_option S_option1" onClick={() => props.onShow2()}>
             <i className="fab fa-buffer"></i>
             <span>Requests</span>
-            {req == 0 ? (
+            {props.getCount() == 0 ? (
               ""
             ) : (
               <i className="fas fa-bell a-set-icon">
-                <span className="a-set-count"> {req}</span>
+                <span className="a-set-count"> {props.getCount()}</span>
               </i>
             )}
           </div>
